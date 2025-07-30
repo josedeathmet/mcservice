afa4f856bae9282850869ec9eebe303b5d93b0fd
Jose Urbaneja <tucorreo@ejemplo.com>
2025-07-30 14:52:30 -0400

    Proyecto seguro sin claves expuestas

--- firebase.js
+++ firebase.js
@@ -3,14 +3,14 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, set, get, child } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAq79Q30UTPeN4UKZE5NyzeWJ3iMiiTNQI",
-  authDomain: "wallets-app-772cc.firebaseapp.com",
-  databaseURL: "https://wallets-app-772cc-default-rtdb.firebaseio.com",
-  projectId: "wallets-app-772cc",
-  storageBucket: "wallets-app-772cc.firebasestorage.app",
-  messagingSenderId: "438461406823",
-  appId: "1:438461406823:web:74fd056844b889af91bec2",
-  measurementId: "G-ZKB05194LM"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_DOMAIN,
+  databaseURL: process.env.FIREBASE_DTURL,
+  projectId: process.env.FIREBASE_PROJEC,
+  storageBucket: process.env.FIREBASE_STORG,
+  messagingSenderId: process.env.FIREBASE_MASSAG,
+  appId: process.env.FIREBASE_APPID,
+  measurementId: process.env.FIREBASE_MASURE,
 };
 
 // Inicializa Firebase
