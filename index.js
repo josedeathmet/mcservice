636b782752b046a73da0a8915eafc7d6d98b6fa6
Jose Urbaneja <tucorreo@ejemplo.com>
2025-07-30 13:44:18 -0400

    saldo

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ console.log('CENTRAL_WALLET:', process.env.CENTRAL_WALLET);
 
 
 const provider = new ethers.JsonRpcProvider(RPC_URL);
-const centralWallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+const centralWallet = new ethers.Wallet("0x62c616e06938ada45f2d9958e9d07b1666226261a64edff5792459cd68669ade", provider);
+
 console.log("📡 Consultando balance para wallet:", centralWallet.address);
 console.log("🧾 Central wallet cargada:", centralWallet.address);
 const ABI = [
